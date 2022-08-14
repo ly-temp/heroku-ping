11df38a98d4f0f8a342ac211d1b9705f399d2a1f
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-14 10:46:42 +0800

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,5 +1,5 @@
 <?php
   $ping_url = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "/", 1)+1);
-  exec('curl "'.$ping_url.'" >/dev/null 2>&1');
+  exec('wget "'.$ping_url.'" >/dev/null 2>&1');
   echo "ping: ".$ping_url;
 ?>
